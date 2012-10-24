5ebd168a7401eec5f725d8a0a37d42f820ed4378
Antoine Turmel <geekshadow@gmail.com>
2012-10-24 16:59:22 +0200

    adding the python make script with instruction in README

---
+++ README.md
@@ -0,0 +1,10 @@
+This repo is dedicated to add-ons which are not relying to Nightingale build system, it will contains add-ons from http://publicsvn.songbirdnest.com/client/extensions/ for example
+
+If you are looking for downloads go here: http://addons.getnightingale.com/
+
+You can use the script make.py to generate xpi files like this:
+```shell
+python make.py <add-on_folder>
+````
+
+
---
+++ make.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from zipfile import ZipFile
+from xml.dom.minidom import parse
+
+if len(sys.argv) == 1:
+  print "specify a folder name in order to build the add-on"
+else:
+  install = parse(os.path.join(sys.argv[1], "install.rdf"))
+  element = install.getElementsByTagName("em:version")[0]
+  version = element.firstChild.data
+  xpiName = sys.argv[1] + "-" + version + ".xpi"
+  xpi = ZipFile(xpiName, "w")
+  os.chdir(sys.argv[1])
+  for root, dirs, files in os.walk("."):
+      for name in files:
+          path = os.path.join(root, name)
+          xpi.write(path, path, 8)
+  xpi.close()
+  os.chdir("..")
+  if os.path.exists(xpiName):
+      print(xpiName + " successfully written.")
+  else:
+      sys.exit(xpiName + " could not be written.")
