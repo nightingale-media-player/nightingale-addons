24b2a124681057dffe0d9bbcd611f725ff5b54ea
darealshinji <djcj@gmx.de>
2014-09-01 07:42:22 +0200

    fix trailing slash problem

--- README.md
+++ README.md
@@ -4,7 +4,5 @@ If you are looking for downloads go here: http://addons.getnightingale.com/
 
 You can use the script make.py to generate xpi files like this:
 ```shell
-python make.py <add-on_folder>
-````
-
-(add-on_folder without trailing slash)
+./make.py <add-on_folder>
+```
--- make.py
+++ make.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 import sys
 from zipfile import ZipFile
@@ -9,7 +11,7 @@ else:
   install = parse(os.path.join(sys.argv[1], "install.rdf"))
   element = install.getElementsByTagName("em:version")[0]
   version = element.firstChild.data
-  xpiName = sys.argv[1] + "-" + version + ".xpi"
+  xpiName = sys.argv[1].replace("/", "") + "-" + version + ".xpi"
   xpi = ZipFile(xpiName, "w")
   os.chdir(sys.argv[1])
   for root, dirs, files in os.walk("."):
